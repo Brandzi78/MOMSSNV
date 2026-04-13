cddd8542ba4084b68910e61cfa094d811841ba7a
Brandzi78 <brano.kalafut78@gmail.com>
2026-04-13 09:25:50 +0200

    main page finished

---
+++ script.js
@@ -0,0 +1,25 @@
+const leftArrows = document.querySelectorAll('.left-arrow');
+const rightArrows = document.querySelectorAll('.right-arrow');
+const wrapper = document.querySelector('.slider-wrapper');
+
+let counter = 0;
+
+// Loop through every right arrow found and give it the instructions
+rightArrows.forEach(button => {
+    button.addEventListener('click', () => {
+        if (counter < 1) { 
+            counter++;
+            wrapper.style.transform = `translateX(-${counter * 50}%)`;
+        }
+    });
+});
+
+// Loop through every left arrow
+leftArrows.forEach(button => {
+    button.addEventListener('click', () => {
+        if (counter > 0) {
+            counter--;
+            wrapper.style.transform = `translateX(-${counter * 50}%)`;
+        }
+    });
+});
